e38a836f53a662dc9a55295d509635380e640dfd
cjeon <cjeon@student.42seoul.kr>
2022-03-01 13:53:38 +0900

    Modify fl

--- scripts/earth.py
+++ scripts/earth.py
@@ -9,9 +9,9 @@ S = False
 N = True
 
 ER = 6371
-R = 4300 + ER
+R = 1700 + ER
 THETA = 37
-PI = 125
+PI = 127
 
 if E:
 	PI += 90
@@ -30,5 +30,5 @@ z = R * math.sin(THETA) * math.sin(PI)
 
 n = math.sqrt(x * x + y * y + z * z)
 od = "%.5f,%.5f,%.5f %.5f,%.5f,%.5f" % (x, y, z, -x / n, -y / n, -z / n)
-print(f"C {od} 60 1.5")
+print(f"C {od} 60 1")
 print(f"li {od} 180 0.8 255,255,255")
